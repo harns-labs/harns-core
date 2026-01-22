13c1a34b96c9d1794e3b35e7c5225e26af6e1ec6
clstr-labs <clstr-labs@users.noreply.github.com>
2026-01-23 01:46:31 +0900

    define SDK constants

---
+++ sdk/src/constants.ts
@@ -0,0 +1,10 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const PROGRAM_ID = new PublicKey("E1iTSqt1YkW6LoNXMspEoxsdotzEdmn3QjEJL5R3NUwe");
+export const CLSTR_MINT = new PublicKey("5jBUVEcMCospWhpNnaePcUy8fnJ6L3FsrN2krdBSdVam");
+export const MAX_RISK_SCORE = 10000;
+export const ZK_HASH_ROUNDS = 256;
+export const CONFIG_SEED = Buffer.from("config");
+export const FLAG_SEED = Buffer.from("flag");
+
+// 6ea9ab1b
